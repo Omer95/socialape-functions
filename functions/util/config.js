5a5056f54b46093b6fb106da726abcd4da632fc6
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2020-04-10 09:37:39 +0100

    refactorig: seperate out routes

---
+++ functions/util/config.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+
+module.exports = {
+    apiKey: functions.config().fbase.key,
+    authDomain: "socialape-c5f2b.firebaseapp.com",
+    databaseURL: "https://socialape-c5f2b.firebaseio.com",
+    projectId: "socialape-c5f2b",
+    storageBucket: "socialape-c5f2b.appspot.com",
+    messagingSenderId: "628954583514",
+    appId: "1:628954583514:web:cf8f3105ffbd0c217830e3",
+    measurementId: "G-SZWHKHG4FW"
+  };
